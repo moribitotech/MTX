61c0749943ceda7f28c92f9c3b590c0009ee111b
Vladimir Yurishchev <vladimir.yurishchev@sap.com>
2013-12-27 17:44:01 +0400

    added android activity for samples

---
+++ maven/android/src/main/java/com/moribitotech/samples/core/MainActivity.java
@@ -0,0 +1,19 @@
+package com.moribitotech.samples.core;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.moribitotech.samples.core.testmain.TestMtxMainGame;
+
+public class MainActivity extends AndroidApplication {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.useGL20 = true;
+        
+        initialize(new TestMtxMainGame(), cfg);
+    }
+}
